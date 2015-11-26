444a9559c2548dd539f19dbff3ca91beb2282063
ADogan <ma@mas-mac-mini.local>
2015-11-26 03:17:45 +0100

    Initial Commit, add readme and cname.

---
+++ js/index.js
@@ -0,0 +1,18 @@
+      $(function () {
+        $('[data-toggle=tooltip]').tooltip();
+        // $('.popsample').popover('hide');
+      });
+//       
+// $("#locationtext").on("click", function(){
+        // $('#locationtext').popover('show');
+		// $(".popover-content").html("<img src='https://maps.googleapis.com/maps/api/staticmap?center=Netherlands&zoom=2&size=150x150&maptype=roadmap' />");
+// });
+
+// $('.collapse').on('shown.bs.collapse', function (e) {
+  // $('.collapse').not(this).removeClass('in');
+// });
+// 
+// $('[data-toggle=collapse]').click(function (e) {
+  // $('[data-toggle=collapse]').parent('li').removeClass('active');
+  // $(this).parent('li').toggleClass('active');
+// });
