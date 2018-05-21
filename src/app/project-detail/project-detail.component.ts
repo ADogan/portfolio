b7ba1d373726a198a178d8d2b7dbc343afd50595
Ali Dogan <alidogan.just.for.commits@gmail.com>
2018-05-21 15:08:59 +0200

    Adjust the project detail component and build

--- src/app/project-detail/project-detail.component.ts
+++ src/app/project-detail/project-detail.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Project } from '../project';
 
 @Component({
   selector: 'app-project-detail',
   templateUrl: './project-detail.component.html',
   styleUrls: ['./project-detail.component.css']
 })
-export class ProjectDetailComponent implements OnInit {
 
+export class ProjectDetailComponent implements OnInit {
+    @Input() project: Project;
   constructor() { }
 
   ngOnInit() {
