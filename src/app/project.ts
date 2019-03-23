0b585bfe85bf8b6e9f7b9003ec9c3b76d29018ba
Ali Dogan <alidogan.just.for.commits@gmail.com>
2019-03-23 05:56:04 +0100

    Change projects style from cards to accordion panels.

--- src/app/project.ts
+++ src/app/project.ts
@@ -3,5 +3,5 @@ export class Project {
     name: string;
     description: string;
     url: string;
-    tags: string;
+    tags: Array<string>;
 }
--- src/app/projects-data.ts
+++ src/app/projects-data.ts
@@ -7,21 +7,23 @@ export const PROJECTS: Project[] = [
         name: 'adogan',
         description: 'This should be the website you are on right now. Its made with Angular 7 and ng bootstrap version 4.',
         url: 'http://www.adogan.nl',
-        tags: 'Angular 7, Bootstrap 4, ng-bootstrap 4'
+        tags: ['Typescript', 'Angular 7', 'Bootstrap 4', 'ng-bootstrap 4']
     }, {
         id: 2,
         name: 'randomprint',
         description: 'Looking for a random word? This simple website gives you a random word following the pattern: '
                         + 'vowel - consonant - vowel - consonant etc.. ',
         url: 'https://www.randomprint.com',
-        tags: 'HTML, CSS, Javascript'
+        tags: [ 'HTML', 'CSS', 'Javascript' ]
     },
     {
         id: 3,
         name: 'verbruikskosten',
-        description: 'Electricity cost of a device calculation page. Meant to quickly perform a simple task without much '
+        description: 'Electricity cost of a device calculation page. (It\'s in Dutch at the moment.) '
+            + 'Meant to quickly perform a simple task without much '
             + 'navigation or confusion for the user. Planned for expanding it with various items for sale on the internet.',
         url: 'https://verbruikskosten.nl',
-        tags: 'HTML, CSS, Javascript, Angular 7, ng-bootstrap 4'
+        tags: [ 'Typescript', 'Angular 7', 'ng-bootstrap 4' ]
+
     }
 ];
